22aa3e6847a45f1299f1773f8e005d23f0a03fcf
lomesshh <78649480+lomesshh@users.noreply.github.com>
2021-03-07 15:54:33 +0530

    API added Sucessfully

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/mandalorian.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("If you ar getting this pop-up then you should know we accept only 5 translates per hour because API which we are using have some limitation OR something wrong with server..!! Please try again after some time THNAK YOU 😉")
+}
 
 function clickHandler() {
-    console.log("Clicked");
-    console.log("input", txtInput.value);
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
